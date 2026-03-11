388c650b1f8ece02e85298dfae84c913e9614487
Nihansh2024 <ashishbadole1987@gmail.com>
2026-03-11 16:26:46 +0530

    Create route.ts

---
+++ src/app/api/facebook/fetch/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export async function POST() {
+
+  return NextResponse.json({
+    comments: [],
+    mediaInfo: {
+      id: "facebook-demo",
+      title: "Facebook Post"
+    }
+  });
+
+}
